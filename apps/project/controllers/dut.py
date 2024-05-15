8ce71004013c4f919f9357eeb115a2ce61901de3
ks3356143 <314298729@qq.com>
2024-05-15 19:07:01 +0800

    998

--- apps/project/controllers/dut.py
+++ apps/project/controllers/dut.py
@@ -11,7 +11,7 @@ from typing import List
 from utils.chen_response import ChenResponse
 from utils.chen_crud import multi_delete_dut
 from utils.codes import HTTP_INDEX_ERROR
-from apps.project.models import Dut, Round, Project
+from apps.project.models import Dut, Round, Project, Design, TestDemand, TestDemandContent, Case, CaseStep
 from django.shortcuts import get_object_or_404
 from apps.project.schemas.dut import DutModelOutSchema, DutFilterSchema, DutTreeReturnSchema, DutTreeInputSchema, \
     DutCreateInputSchema, DutCreateOutSchema, DeleteSchema, DutCreateR1SoDutSchema
@@ -140,7 +140,7 @@ class DutController(ControllerBase):
         else:
             return ChenResponse(data={'exists': False}, status=200)
 
-    # 弹窗添加第一轮被测件源代码信息
+    # 弹窗添加第一轮被测件源代码信息，另外创建测试项（静态分析、代码审查），测试用例（静态分析、代码审查）
     @route.post("/dut/createR1Sodut", response=DutCreateOutSchema, url_name='dut-r1SoDut')
     @transaction.atomic
     def create_r1_so_dut(self, data: DutCreateR1SoDutSchema):
@@ -156,5 +156,152 @@ class DutController(ControllerBase):
         round_id = project_obj.pField.filter(key='0').first().id
         asert_dict['round_id'] = round_id
         asert_dict.update({'key': key_string, 'title': '软件源代码', 'type': 'SO', 'name': '软件源代码', 'level': '1'})
-        qs = Dut.objects.create(**asert_dict)
-        return qs
+        dut_qs: Dut = Dut.objects.create(**asert_dict)
+        # 到这里就逼迫用户创建了第一轮的so_dut
+        # 1.现在要创建静态分析和代码审查设计需求（design2个）
+        user_name = self.context.request.user.name
+        # 1.1.自动创建design静态分析
+        jt_design_create_dict = {
+            'ident': 'JTFX',
+            'name': '静态分析',
+            'demandType': '6',
+            'description': "对源代码程序进行静态分析",
+            'title': '静态分析',
+            'key': ''.join([dut_qs.key, '-', '0']),
+            'level': '2',
+            'chapter': '/',
+            'project': project_obj,
+            'round': dut_qs.round,
+            'dut': dut_qs
+        }
+        new_design_jt: Design = Design.objects.create(**jt_design_create_dict)
+        # 1.1.1.自动创建demand静态分析
+        jt_demand_create_dict = {
+            'ident': 'JTFX',
+            'name': '静态分析',
+            'adequacy': '对软件全部源程序进行进行质量度量、控制流分析、数据流分析的静态统计信息分析',
+            'priority': '2',
+            'testType': '15',
+            'testMethod': '["3"]',
+            'title': '静态分析',
+            'key': ''.join([new_design_jt.key, '-', '0']),
+            'level': '3',
+            'project': project_obj,
+            'round': new_design_jt.round,
+            'dut': new_design_jt.dut,
+            'design': new_design_jt,
+        }
+        new_demand_jt = TestDemand.objects.create(**jt_demand_create_dict)
+        TestDemandContent.objects.create(testDemand=new_demand_jt, subName='静态分析',
+                                         subDesc='对被测软件全部源程序进行静态分析，'
+                                                 '对控制流、数据流进行分析，验证软件是否满足控制流和数据流要求。'
+                                                 '依据质量特性需求统计质量度量信息',
+                                         condition='已有被测件全部源代码',
+                                         operation='使用LDRA '
+                                                   'TestBed软件和Klocwork软件工具对被测软件全部源'
+                                                   '程序进行静态分析，依据附录5对源程序进行检查。'
+                                                   '\a1）使用静态分析工具统计软件质量度量信息，包含：'
+                                                   '\a（1）软件总注释率不小于20'
+                                                   '%（注释行数/软件规模*100%）；\a（2）'
+                                                   '模块的平均规模不大于200行（模块代码行数之和/模块数）'
+                                                   '；\a（3）模块的平均圈复杂度不大于10（模块圈复杂度之和/模块总数）'
+                                                   '；\a（4）	'
+                                                   '模块的平均扇出数不大于7（模块扇出数之和/模块总数）。'
+                                                   '\a2）使用静态分析工具结合人工分析对控制流和数据流进行分析，'
+                                                   '验证软件是否满足控制流和数据流要求。', )
+        new_case_jt = Case.objects.create(
+            ident='JTFX',
+            name='静态分析',
+            initialization='已获取全部被测件源代码程序，静态分析工具准备齐备',
+            premise='提交的代码出自委托方受控库，是委托方正式签署外发的',
+            summarize='依据委托方的要求进行静态分析，验证软件质量度量和编码规则是否满足军标要求',
+            designPerson=user_name,
+            testPerson=user_name,
+            monitorPerson=user_name,
+            project=project_obj,
+            isLeaf=True,
+            round=new_demand_jt.round,
+            dut=new_demand_jt.dut,
+            design=new_demand_jt.design,
+            test=new_demand_jt,
+            title='静态分析',
+            key=''.join([new_demand_jt.key, '-', '0']),
+            level='4'
+        )
+        CaseStep.objects.create(case=new_case_jt,
+                                operation='使用LDRA TestBed软件和Klocwork软件工具对被测软件'
+                                          '全部源程序进行静态分析，并配合人工以及检查单进行分析',
+                                expect='静态审查单全部通过，且源代码满足编码规则和质量度量要求',
+                                result='静态度量结果符合国军标要求，静态分析审查单全部通过', )
+        # 1.2.自动创建代码审查design
+        dm_design_create_dict = {
+            'ident': 'DMSC',
+            'name': '代码审查',
+            'demandType': '6',
+            'description': "对源码代码程序进行人工审查",
+            'title': '代码审查',
+            'key': ''.join([dut_qs.key, '-', '1']),
+            'level': '2',
+            'chapter': '/',
+            'project': dut_qs.project,
+            'round': dut_qs.round,
+            'dut': dut_qs
+        }
+        new_design_dm = Design.objects.create(**dm_design_create_dict)
+        dm_demand_create_dict = {
+            'ident': 'DMSC',
+            'name': '代码审查',
+            'adequacy': '对软件全部源代码/重点模块进行代码审查',
+            'priority': '2',
+            'testType': '2',
+            'testMethod': '["3"]',
+            'title': '代码审查',
+            'key': ''.join([new_design_dm.key, '-', '0']),
+            'level': '3',
+            'project': project_obj,
+            'round': new_design_dm.round,
+            'dut': new_design_dm.dut,
+            'design': new_design_dm,
+        }
+        new_demand_dm = TestDemand.objects.create(**dm_demand_create_dict)
+        TestDemandContent.objects.create(
+            testDemand=new_demand_dm,
+            subName='代码审查',
+            subDesc='通过人工审查及借助工具辅助分析的方式开展代码审查，审查代码编程准则的符合性、'
+                    '代码流程实现的正确性、代码结构的合理性以及代码实现需求的正确性；人工审查中发现的问题，审查人员应及时记录',
+            condition='被测件源代码已获取',
+            operation='人工审查及借助工具辅助分析的方式',
+            observe='和依据附录代码审查单范围内的源代码开展四个方面的审查:\a',
+            expect='1）编程准则检查：依据编程准则的要求，对程序的编码与编程准则进行符合性检查；\a'
+                   '2）代码流程审查：审查程序代码的条件判别、控制流程、数据处理等满足设计要求；\a'
+                   '3）软件结构审查：依据设计文档，审查程序代码的结构设计的合理性，包括程序结构设计和数据结构设计；\a'
+                   '4）需求实现审查：依据需求文档及其他相关资料，审查程序代码的需求层的功能实现是否正确；\a'
+        )
+        new_case_dm = Case.objects.create(
+            ident='DMSC',
+            name='代码审查',
+            initialization='代码已提交',
+            premise='提交的代码出自委托方受控库，是委托方正式签署外发的',
+            summarize='通过人工审查及借助工具辅助分析的方式开展代码审查，审查代码编程准则的符合性、'
+                      '代码流程实现的正确性、代码结构的合理性以及代码实现需求的正确性；人工审查中发现的问题，审查人员应及时记录',
+            designPerson=user_name,
+            testPerson=user_name,
+            monitorPerson=user_name,
+            project=project_obj,
+            isLeaf=True,
+            round=new_demand_dm.round,
+            dut=new_demand_dm.dut,
+            design=new_demand_dm.design,
+            test=new_demand_dm,
+            title='代码审查',
+            key=''.join([new_demand_dm.key, '-', '0']),
+            level='4'
+        )
+        CaseStep.objects.create(case=new_case_dm,
+                                operation='通过人工审查及借助工具辅助分析的方式开展代码审查，审查代码编程准则的符合性、'
+                                          '代码流程实现的正确性、代码结构的合理性以及代码实现需求的正确性；'
+                                          '人工审查中发现的问题，审查人员应及时记录',
+                                expect='代码设计正确，满足审查单要求，无不符合项',
+                                result='代码设计正确，满足审查单要求，无不符合项', )
+
+        return dut_qs
